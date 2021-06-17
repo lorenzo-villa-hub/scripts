98417f32fa1ab2f209e6cc0618815c4352538b24
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2021-06-17 17:39:02 +0200

    Updated run_job args and added only sync

--- run_job.py
+++ run_job.py
@@ -22,8 +22,9 @@ class RunJob:
         parser = ap.ArgumentParser()
         
         parser.add_argument('-j','--jobclass',help='Job class, default is VaspJob',required=False,default='VaspJob',type=str,metavar='',dest='jobclass')
-        parser.add_argument('-s','--sync',help='sync to hpc',required=False,default=True,type=str,metavar='',dest='sync')
+        parser.add_argument('-s','--sync',help='sync to hpc',required=False,default=False,action='store_true',dest='sync')
         parser.add_argument('-f','--filename',help='job_script_filename',required=False,default=None,type=str,metavar='',dest='filename')
+        parser.add_argument('-R','--run',help='Run job',required=False,default=False,action='store_true',dest='run')
         
         args = parser.parse_args()
         
@@ -33,14 +34,25 @@ class RunJob:
                 
         return
     
-    def run(self):   
+    def run_job(self):   
         cls = globals()[self.jobclass]
         j = cls.from_directory('.',job_script_filename=self.filename,load_outputs=False)
-        j.run_job(write_input=False,sync=self.sync)
+        if self.run:
+            j.run_job(write_input=False,sync=self.sync)
         return
+ 
+    def sync_to_hpc(self):
+        cls = globals()[self.jobclass]
+        j = cls.from_directory('.',job_script_filename=self.filename,load_outputs=False)
+        if self.sync:
+           j.sync_to_hpc()
+           return
     
 
 if __name__ == '__main__':
     r = RunJob()
     r.args()
-    r.run()
+    if r.run:
+        r.run_job()
+    elif r.sync:
+        r.sync_to_hpc()
